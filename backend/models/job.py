a69fe1e56f748d55da2ce5018e4dee907b031d88
TashifToor <tashiftoor12345@gmail.com>
2026-06-30 17:21:03 +0500

    update in models

--- backend/models/job.py
+++ backend/models/job.py
@@ -1,17 +1,17 @@
-# import uuid
-# from sqlalchemy import Column, String, Text, Integer, DateTime, ForeignKey, Boolean
-# from sqlalchemy.sql import func
-# from sqlalchemy.dialects.postgresql import UUID
-# from models.database import Base
+import uuid
+from sqlalchemy import Column, String, Text, Integer, DateTime, ForeignKey, Boolean
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import UUID
+from models.database import Base
 
-# class Job(Base):
-#     __tablename__="jobs"
+class Job(Base):
+    __tablename__ = "jobs"
 
-#     id=          Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4,index=True)
-#     hr_user_id=  Column(Integer,ForeignKey("user.id"),nullable=False)
-#     title=       Column(String,nullable=False)
-#     company=     Column(String,nullable=True)
-#     location=    Column(String,nullable=True)
-#     description= Column(Text,nullable=False)
-#     is_active=   Column(Boolean,default=True)
-#     created_at=  Column(DateTime(timezone=True),server_default=func.now())
+    id =          Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    hr_user_id =  Column(Integer, ForeignKey("users.id"), nullable=False)
+    title =       Column(String, nullable=False)
+    company =     Column(String, nullable=True)
+    location =    Column(String, nullable=True)
+    description = Column(Text, nullable=False)
+    is_active =   Column(Boolean, default=True)
+    created_at =  Column(DateTime(timezone=True), server_default=func.now())
