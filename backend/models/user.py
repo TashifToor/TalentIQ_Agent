6d67e571f20639d7f017af904ba25d580ecc64d4
TashifToor <tashiftoor12345@gmail.com>
2026-07-07 16:10:29 +0500

    add & improve user

--- backend/models/user.py
+++ backend/models/user.py
@@ -8,6 +8,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
+    normalized_email = Column(String, unique=True, index=True, nullable=True)
     password_hash = Column(String, nullable=False, unique=True)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.utcnow)
