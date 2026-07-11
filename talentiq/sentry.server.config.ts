e3aca0cc4b497131e59b5d536d38cff46d908b58
TashifToor <tashiftoor12345@gmail.com>
2026-07-11 20:10:35 +0500

    remove sentry

--- talentiq/sentry.server.config.ts
+++ talentiq/sentry.server.config.ts
@@ -1,12 +1,12 @@
-// This file configures the Sentry SDK for server-side rendering / Route Handlers.
-import * as Sentry from "@sentry/nextjs";
+// // This file configures the Sentry SDK for server-side rendering / Route Handlers.
+// import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+// const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-if (SENTRY_DSN) {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    environment: process.env.NEXT_PUBLIC_ENVIRONMENT || "development",
-    tracesSampleRate: 0.2,
-  });
-}
+// if (SENTRY_DSN) {
+//   Sentry.init({
+//     dsn: SENTRY_DSN,
+//     environment: process.env.NEXT_PUBLIC_ENVIRONMENT || "development",
+//     tracesSampleRate: 0.2,
+//   });
+// }
